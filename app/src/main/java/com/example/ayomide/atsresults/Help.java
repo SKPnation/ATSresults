9b41b2dccb6ecc328c77e57dff068792231dad7a
SKPnation <ayomideseaz@gmail.com>
2019-08-05 18:35:24 +0100

    made some modifications to the code

--- app/src/main/java/com/example/ayomide/atsresults/Help.java
+++ app/src/main/java/com/example/ayomide/atsresults/Help.java
@@ -2,19 +2,39 @@ package com.example.ayomide.atsresults;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.UserHandle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.ayomide.atsresults.Common.Common;
+import com.example.ayomide.atsresults.Model.Parent;
+import com.example.ayomide.atsresults.Model.User;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.jaredrummler.materialspinner.MaterialSpinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Help extends AppCompatActivity {
 
-    EditText etRecipient, etSubject, etMessage;
+    EditText etRecipient, etSubject, etMessage, adminPhone;
     Button btnSend;
+    FloatingActionButton fab;
+    MaterialSpinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +57,47 @@ public class Help extends AppCompatActivity {
                 sendEmail(recipient, subject, message);
             }
         } );
+
+        fab = findViewById( R.id.fab_call );
+        fab.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog();
+            }
+        } );
+    }
+
+    private void showDialog() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder( Help.this );
+        alertDialog.setTitle( "Call admin" );
+
+        LayoutInflater inflater = this.getLayoutInflater();
+        View admin_option_layout = inflater.inflate( R.layout.admin_option_layout, null );
+
+
+        adminPhone =  admin_option_layout.findViewById(R.id.admin_phone);
+
+        alertDialog.setView( admin_option_layout );
+
+        //Load all admin phone to spinner
+        final List<String> adminList = new ArrayList<>();
+        FirebaseDatabase.getInstance().getReference("User")
+                .addListenerForSingleValueEvent( new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for (DataSnapshot adminSnapshot:dataSnapshot.getChildren())
+                            adminList.add( adminSnapshot.getKey() );
+                        adminPhone.setText( adminList.get( 0 ) );
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                } );
+
+
+        alertDialog.show();
     }
 
     private void sendEmail(String recipient, String subject, String message)
